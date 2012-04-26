1dcf5cb6b9a58b216d7186e02a08640c46062342
Dan Chuparkoff <Dan@Chuparkoff.com>
2012-04-26 17:15:57 -0500

    Checking out checkout from within NetBeans. Added, now committing.

--- lib/exercises/week2/w2c3_column_string_assignment.rb
+++ lib/exercises/week2/w2c3_column_string_assignment.rb
@@ -10,10 +10,11 @@
   This program was created as an exercise in the RubyLearning PORPC101-32C Course.
   http://rubylearning.com
 
-  This program illustrates the use of the .split method combined with the .each_index method.  
+  This program illustrates the use of the .split method combined with the .each_index method.
 =end
 
-# A line splitting method, parsing a string on each newline using .split and putting the result back out with line numbers.
+# A line splitting method, parsing a string on each newline using .split and putting the result back out
+# with line numbers.
 def line_split (in_string)
   #split the incoming string on each newline
   my_string_array = in_string.split("\n")
@@ -23,8 +24,4 @@ def line_split (in_string)
     puts "Line #{index + 1}: #{my_string_array[index]}"
   end
 end
-
-
-# Main ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
